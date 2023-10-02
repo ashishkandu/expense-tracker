0a153df019d68472164d46b567aabb8ed5b5d2ba
ashishkandu <ashishkandu43@gmail.com>
2023-10-02 09:59:25 +0545

    expense list component created

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import "./index.css";
+import ExpenseList from "./components/ExpenseList";
+import { useState } from "react";
+
+function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
+  return (
+    <>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
+    </>
+  );
+}
+
+export default App;
